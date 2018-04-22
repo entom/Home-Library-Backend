873a14bccebf6c610fa2f920f78979633c395b6e
entom <tomasz@entom.info>
2018-04-22 20:29:49 +0200

    prepared documentation for index method in API, updated README file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.json({status: 'ok'});
+/**
+ * @swagger
+ * definition:
+ *   Index:
+ *     properties:
+ *       status:
+ *         type: string
+ * @swagger
+ * /api/index:
+ *   get:
+ *     description: Check status of API
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: JSON with status
+ *         schema:
+ *           $ref: '#/definitions/Index'
+ */
+router.get('/', function (req, res, next) {
+    res.json({status: 'ok'});
 });
 
 module.exports = router;
