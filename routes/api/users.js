5bfd0e336ab3cc618a42105ea1b1e394047f5c60
entom <tomasz@entom.info>
2018-04-22 21:31:15 +0200

    prepare post for users api

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
+    res.json({});
+});
+
+router.post('/', (req, res, next) => {
     res.json({});
 });
 
