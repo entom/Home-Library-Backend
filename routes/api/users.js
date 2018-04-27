2ae726d116d9c49545392338b432826f8a911672
Tomasz Nicieja <tomasznicieja@gmail.com>
2018-04-28 00:07:32 +0200

    prepared simple user schema and API method for create user and list users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,18 +1,52 @@
 let express = require('express')
 let router = express.Router()
+let bodyParser = require('body-parser')
+
+router.use(bodyParser.urlencoded({extended: true}))
+router.use(bodyParser.json())
 
 /**
  *
+ * @type {module:mongoose.Model<module:mongoose.Document>|*}
+ */
+let User = require('./../../models/User')
+
+/**
+ * @swagger
+ * /api/users/:
+ *   get:
+ *     tags:
+ *       - Users
+ *     description: Get list of users
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: JSON with array of users
+ *         schema:
+ *           type: array
+ *           items:
+ *             $ref: '#/definitions/User'
  */
 router.get('/', (req, res) => {
-  res.json({})
+  User.find({}, (err, users) => {
+    if (err) return res.status(400).send('There was a problem finding the users.')
+    res.status(200).send(users)
+  })
 })
 
 /**
  *
  */
 router.post('/', (req, res) => {
-  res.json({})
+  User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  }, (err, user) => {
+    if (err) return res.status(400).send('There was a problem adding the information to the database.')
+    res.status(200).send(user)
+  })
 })
 
 module.exports = router
