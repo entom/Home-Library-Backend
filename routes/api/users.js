3d74661f1cd4dc76de7c2e07954dae0caaa74e95
Tomasz Nicieja <tomasznicieja@gmail.com>
2018-04-28 00:28:49 +0200

    users api - delete single user, added swagger comments

--- routes/api/users.js
+++ routes/api/users.js
@@ -131,4 +131,31 @@ router.put('/:id', (req, res) => {
   })
 })
 
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   delete:
+ *     tags:
+ *       - Users
+ *     description: Delete user
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: User ID number
+ *         required: true
+ *         type: string
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Message
+ *         type: string
+ */
+router.delete('/:id', function (req, res) {
+  User.findByIdAndRemove(req.params.id, (err, user) => {
+    if (err) return res.status(500).send('There was a problem deleting the user.')
+    res.status(200).send('User ' + user.name + ' was deleted.')
+  })
+})
+
 module.exports = router
