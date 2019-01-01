05cd56451c3a004af19211ebf89f485403707213
entom <tomasz@entom.info>
2019-01-01 21:47:17 +0100

    building authentication for dashboard

--- routes/panel/login.js
+++ routes/panel/login.js
@@ -1,8 +1,21 @@
 let express = require('express')
 let router = express.Router()
+let User = require('./../../models/User')
 
 router.get('/', (req, res) => {
   res.render('panel/login/index')
+})
+
+router.post('/', (req, res) => {
+  User.findOne({email: req.body.email}, (err, user) => {
+    if (err) {
+      res.redirect('/panel/login')
+    } else if (!user) {
+      res.redirect('/panel/login')
+    } else {
+
+    }
+  })
 })
 
 module.exports = router
